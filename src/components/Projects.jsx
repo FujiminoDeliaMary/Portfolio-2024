2084b7c09e76fc3eb8f06e0799df989e67f90dfd
TSENE <marilynedelia@gmail.com>
2024-04-19 19:39:21 +0200

    mise en prod

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,73 +1,72 @@
-import React, { useState } from "react";
-import portfolio from '../assets/images/portfolio.png'
-import artfullCode from '../assets/images/artfullcode.png'
-import lettreenlumiere from '../assets/images/lettreenlumiere.png'
-import nextIcon from '../assets/icons/next.svg'
-import githubIcon from '../assets/icons/github.svg'
-import  {useTranslation} from 'react-i18next'
-
-
+import React, { useState, useEffect } from "react";
+import { useTranslation } from 'react-i18next';
+import portfolio from '../assets/images/portfolio.png';
+import artfullCode from '../assets/images/artfullcode.png';
+import lettreenlumiere from '../assets/images/lettreenlumiere.png';
+import nextIcon from '../assets/icons/next.svg';
+import githubIcon from '../assets/icons/github.svg';
 
 const Projects = () => {
+    const { t } = useTranslation();
 
-    const {t} = useTranslation();
-    const [projects, setProjects] = useState([
+
+    const [projects] = useState([
         {
-            image : portfolio,
+            image: portfolio,
+            titleKey: "portfolio",
             title : 'Portfolio',
-            description : t("portfolioDescription"),
-            githubLink : 'https://github.com/FujiminoDeliaMary/Portfolio-2024',
-            link : 'https://mtsene.lpmiaw.univ-lr.fr/portfolio/',
-            technos : ['#ReactJs', '#Scss', '#Framer Motion', '#i18next']
+            githubLink: 'https://github.com/FujiminoDeliaMary/Portfolio-2024',
+            link: 'https://mtsene.lpmiaw.univ-lr.fr/portfolio/',
+            technos: ['#ReactJs', '#Scss', '#Framer Motion', '#i18next']
         },
         {
-            image : artfullCode,
-            title : 'ArtFull Code',
-            description : t("artfullCodeDescription"),
-            githubLink : 'https://github.com/FujiminoDeliaMary/ArtfullCode',
-            link : 'https://agence-artfull-code.lpmiaw.univ-lr.fr/',
-            technos : ['#ReactJs', '#Scss']
+            image: artfullCode,
+            titleKey: "artfullCode",
+            title : 'Artfull Code',
+            githubLink: 'https://github.com/FujiminoDeliaMary/ArtfullCode',
+            link: 'https://agence-artfull-code.lpmiaw.univ-lr.fr/',
+            technos: ['#ReactJs', '#Scss']
         },
         {
-            image : lettreenlumiere,
+            image: lettreenlumiere,
+            titleKey: "lettresEnLumiere",
             title : 'Lettres en lumière',
-            description : t("lettresEnLumiereDescription"),
-            githubLink : '/',
-            link : 'https://mtsene.lpmiaw.univ-lr.fr/lettrelumierefront/',
-            technos : ['#ReactJs', '#Scss', '#Framer Motion', '#Symfony']
-        },
-
+            githubLink: '/',
+            link: 'https://mtsene.lpmiaw.univ-lr.fr/lettrelumierefront/',
+            technos: ['#ReactJs', '#Scss', '#Framer Motion', '#Symfony']
+        }
     ]);
 
-    return <section className="projects" id="projects">
-                <h2 className="projects__title">{`${t("projectsTitle")}`}</h2>
-                <h3 className="projects__subtitle">{`${t("projectsSubtitle")}`}</h3>
-                
-                <div className="projects__list">
-                    {
-                        projects.map((project, index) => {
-
-                            return <>
-                                        <div className="project" key={index}>
-                                                <img src={project.image} alt=""className="project__image"/>
-                                                <div className="project__top">
-                                                    <p className="project__title">{project.title}</p>
-                                                    <div className="project__links">
-                                                        <a href={project.link} target="_blank" rel="noopener noreferrer"><img src={nextIcon} alt="Aller vers le site" className="project__link project__link--next" /></a>
-                                                        <a href={project.githubLink} target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="Aller vers le site" className="project__link project__link--github"/></a>
-                                                    </div>
-                                                </div>
-                                                <p className="project__description">
-                                                    {project.description}
-                                                </p>
-                                                <p className="project__technos">{project.technos.join(', ')}</p>
-                                        </div>
-                                    </>
-                        })
-                    }
-                </div>
-  
-            </section>
+    return (
+        <section className="projects" id="projects">
+            <h2 className="projects__title">{t("projectsTitle")}</h2>
+            <h3 className="projects__subtitle">{t("projectsSubtitle")}</h3>
+            
+            <div className="projects__list">
+                {/* Boucle pour display tous les projets */}
+                {projects.map((project, index) => (
+                    <div className="project" key={index}>
+                        <img src={project.image} alt={t(`${project.titleKey}`)} className="project__image"/>
+                        <div className="project__top">
+                            <p className="project__title">{t(`${project.title}`)}</p>
+                            <div className="project__links">
+                                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                    <img src={nextIcon} alt="Go to site" className="project__link project__link--next" />
+                                </a>
+                                <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                                    <img src={githubIcon} alt="Go to GitHub" className="project__link project__link--github" />
+                                </a>
+                            </div>
+                        </div>
+                        <p className="project__description">
+                            {t(`${project.titleKey}Description`)}
+                        </p>
+                        <p className="project__technos">{project.technos.join(', ')}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
 
 export default Projects;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,72 +1,46 @@
-import React, { useState } from "react";
-import backendIcon from '../assets/images/backend.png'
-import frontendIcon from '../assets/images/front-end.jpg'
-import bdIcon from '../assets/images/bd.png'
-import github from '../assets/images/github.png'
-import uxdesignIcon from '../assets/images/UX.png'
-import wordpressIcon from '../assets/images/wordpress.png'
-import { Zoom } from "react-awesome-reveal"
-import  {useTranslation} from 'react-i18next'
-
+import React from "react";
+import backendIcon from '../assets/images/backend.png';
+import frontendIcon from '../assets/images/front-end.jpg';
+import bdIcon from '../assets/images/bd.png';
+import github from '../assets/images/github.png';
+import uxdesignIcon from '../assets/images/UX.png';
+import wordpressIcon from '../assets/images/wordpress.png';
+import { Zoom } from "react-awesome-reveal";
+import { useTranslation } from 'react-i18next';
 
 const Skills = () => {
+    const { t } = useTranslation();
 
-    const {t} = useTranslation();
-    const [skills, setSkills] = useState([
-                                            {
-                                                img: backendIcon,
-                                                title : t("backendDevelopment"),
-                                                description: t("backendDescription")
-                                            },
-                                            {
-                                                img : frontendIcon,
-                                                title : t("frontendDevelopment"),
-                                                description: t("frontendDescription")
-                                            },
-                                            {
-                                                img : bdIcon,
-                                                title : t("database"),
-                                                description: t("databaseDescription")
-                                            },
-                                            {
-                                                img : github,
-                                                title : t("versionControl"),
-                                                description: t("versionControlDescription")
-                                            },
-                                            {
-                                                img : wordpressIcon,
-                                                title : t("wordpress"),
-                                                description: t("wordpressDescription")
-                                            },
-                                            {
-                                                img : uxdesignIcon,
-                                                title : t("uxuiDesign"),
-                                                description:t("uxuiDescription")
-                                            }
-                                        ])
-    return <section className="skills" id="skills">
-            <h2 className="skills__title">{`${t("skillsTitle")}`}</h2>
+
+    const skillsInfo = [
+        { img: backendIcon, titleKey: "backendDevelopment", descriptionKey: "backendDescription" },
+        { img: frontendIcon, titleKey: "frontendDevelopment", descriptionKey: "frontendDescription" },
+        { img: bdIcon, titleKey: "database", descriptionKey: "databaseDescription" },
+        { img: github, titleKey: "versionControl", descriptionKey: "versionControlDescription" },
+        { img: wordpressIcon, titleKey: "wordpress", descriptionKey: "wordpressDescription" },
+        { img: uxdesignIcon, titleKey: "uxuiDesign", descriptionKey: "uxuiDescription" }
+    ];
+
+    return (
+        <section className="skills" id="skills">
+            <h2 className="skills__title">{t("skillsTitle")}</h2>
             
             <div className="skills__list">
-                {
-                    skills.map((skill, index) => {
-                        return   <figure className="skill" key={index}>
-                                        <Zoom triggerOnce={true}>
-                                            <img src={skill.img} alt="" className="skill__image"/>
-                                            <figcaption>
-                                                <p className="skill__title">{skill.title}</p>
-                                                <p className="skill__description">{skill.description}</p>
-                                            </figcaption>
-                                        </Zoom>
-                                    </figure>
-                     
-                      
-                    })
-                }
+                {/*  */}
+                {skillsInfo.map((skill, index) => (
+                    <figure className="skill" key={index}>
+                        <Zoom triggerOnce={true}>
+                            <img src={skill.img} alt={t(skill.titleKey)} className="skill__image" />
+                            <figcaption>
+                                <p className="skill__title">{t(skill.titleKey)}</p>
+                                <p className="skill__description">{t(skill.descriptionKey)}</p>
+                            </figcaption>
+                        </Zoom>
+                    </figure>
+                ))}
             </div>
-      
-         
         </section>
-}
+    );
+};
 
 export default Skills;
